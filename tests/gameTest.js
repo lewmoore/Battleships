70b284a92f70e4507ade901d3dc73c878f22692b
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-20 13:55:19 +0100

    Replace board array with numbers

--- tests/gameTest.js
+++ tests/gameTest.js
@@ -7,6 +7,6 @@ describe('game', function(){
   })
 
   it('initialises with a board', function(){
-    expect(game.board).toEqual(['A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5', 'E1', 'E2', 'E3', 'E4', 'E5'])
+    expect(game.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
   })
 })
