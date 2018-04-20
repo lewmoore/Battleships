4296944bdf748c7135974dd47d8683ee9630c392
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-20 14:20:40 +0100

    Add placeShip function which replaces array element with a ship

--- src/game.js
+++ src/game.js
@@ -2,4 +2,8 @@ class Game {
   constructor(){
     this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
   }
+
+  placeShip(target){
+    this.board.splice(target -1, 1, 'ship')
+  }
 }
--- tests/gameTest.js
+++ tests/gameTest.js
@@ -9,4 +9,9 @@ describe('game', function(){
   it('initialises with a board', function(){
     expect(game.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
   })
+
+  it('places a ship on the board', function(){
+    game.placeShip(1)
+    expect(game.board).toContain('ship')
+  })
 })
