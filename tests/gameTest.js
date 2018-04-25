26055f852d2501b48cc0b2166d47ac745f34acf4
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-25 10:07:15 +0100

    Refactor tests to arrow functions

--- tests/gameTest.js
+++ tests/gameTest.js
@@ -6,29 +6,29 @@ describe('game', function(){
     game = new Game()
   })
 
-  it('initialises with a board', function(){
+  it('initialises with a board', () => {
     expect(game.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
   })
 
-  it('places a ship on the board', function(){
+  it('places a ship on the board', () => {
     game.placeShip(1)
     expect(game.board).toContain('Ship')
   })
 
-  it('initialises with an empy currentShip', function(){
+  it('initialises with an empy currentShip', () => {
     expect(game.currentShip).toEqual('')
   })
 
-  it('has an array of ships', function(){
+  it('has an array of ships', () => {
     expect(game.ships).toEqual(['Small Ship', 'Medium Ship', 'Large Ship'])
   })
 
-  it('a ship can be selected and changes currentShip', function(){
+  it('a ship can be selected and changes currentShip', () => {
     game.selectShip('Small Ship')
     expect(game.currentShip).toEqual('Small Ship')
   })
 
-  it('medium ship takes up 3  spaces', function(){
+  it('medium ship takes up 3  spaces', () => {
     game.selectShip('Medium Ship')
     game.placeShip(1)
     expect(game.board[0]).toEqual('Medium Ship')
@@ -36,7 +36,7 @@ describe('game', function(){
     expect(game.board[2]).toEqual('Medium Ship')
   })
 
-  it('Large Ship takes up 5 spaces', function(){
+  it('Large Ship takes up 5 spaces', () => {
     game.selectShip('Large Ship')
     game.placeShip(1)
     expect(game.board[0]).toEqual('Large Ship')
