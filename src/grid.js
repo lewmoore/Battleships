422f44e79b7f18c4fcecf9ce1520d380a5380fb6
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-25 14:46:25 +0100

    Add other three rows for the grid class

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,9 @@
-class Grid{
-  constructor(){
-    this.gridA = ['A1', 'A2', 'A3', 'A4', 'A5']
-    this.gridB = ['B1', 'B2', 'B3', 'B4', 'B5']
+class Grid {
+  constructor() {
+    this.gridA = ["A1", "A2", "A3", "A4", "A5"];
+    this.gridB = ["B1", "B2", "B3", "B4", "B5"];
+    this.gridC = ["C1", "C2", "C3", "C4", "C5"];
+    this.gridD = ["D1", "D2", "D3", "D4", "D5"];
+    this.gridE = ["E1", "E2", "E3", "E4", "E5"];
   }
 }
--- tests/gridTest.js
+++ tests/gridTest.js
@@ -1,14 +1,25 @@
-describe('Grid', () => {
-
+describe("Grid", () => {
   beforeEach(() => {
-    grid = new Grid()
-  })
+    grid = new Grid();
+  });
 
-  it('"A" array initiliases with 5 elements', () => {
-    expect(grid.gridA).toEqual(['A1', 'A2', 'A3', 'A4', 'A5'])
-  })
+  it("gridA initialises with 5 elements", () => {
+    expect(grid.gridA).toEqual(["A1", "A2", "A3", "A4", "A5"]);
+  });
 
-  it('gridB initialises with 5 elements', () => {
-    expect(grid.gridB).toEqual(['B1', 'B2', 'B3', 'B4', 'B5'])
-  })
-})
+  it("gridB initialises with 5 elements", () => {
+    expect(grid.gridB).toEqual(["B1", "B2", "B3", "B4", "B5"]);
+  });
+
+  it("gridC initialises with 5 elements", () => {
+    expect(grid.gridC).toEqual(["C1", "C2", "C3", "C4", "C5"]);
+  });
+
+  it("gridD initialises with 5 elements", () => {
+    expect(grid.gridD).toEqual(["D1", "D2", "D3", "D4", "D5"]);
+  });
+
+  it("gridE initialises with 5 elements", () => {
+    expect(grid.gridE).toEqual(["E1", "E2", "E3", "E4", "E5"]);
+  });
+});
