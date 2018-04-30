ac6915de5783a33c80f7d0cd8daff8614e2a8320
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-30 10:18:14 +0100

    Game board initialises with instance of Grid instead of hardcoded array

--- tests/gameTest.js
+++ tests/gameTest.js
@@ -3,10 +3,11 @@ describe("game", function() {
 
   beforeEach(function() {
     game = new Game();
+    grid = new Grid()
   });
 
-  it("initialises with a board", () => {
-    expect(game.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]);
+  it("initialises with an instance of Grid as the board", () => {
+    expect(game.board).toEqual(grid);
   });
 
   it('a ship can be selected and changes currentShip', () => {
