18c4cadb943bf40c57fdb49364cb38a6cf3321f0
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-01 11:08:07 +0100

    Remove commented code

--- src/game.js
+++ src/game.js
@@ -8,17 +8,6 @@ class Game {
     this.currentShip = ship
   }
 
-  // placeShip(ship, target){
-  //   var index = this.board.grid.findIndex(element => element == target)
-  //   if (ship.size == 2){
-  //     this.board.grid.splice(index, ship.size, 'X', 'X')
-  //   } else if (ship.size == 3 ){
-  //     this.board.grid.splice(index, ship.size, 'X', 'X', 'X')
-  //   } else {
-  //     this.board.grid[index] = 'X'
-  //   }
-  // }
-  //
   placeShip(ship, target){
     var index = this.board.grid.findIndex(element => element == target)
     for (var i = 0; i < ship.size; i++){
