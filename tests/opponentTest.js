9f2a290b57fac225ae4bb26aadd8cd76848ccf90
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-04 14:39:56 +0100

    Add spy to opponent test. Opponent class has two ships on board on creation

--- tests/opponentTest.js
+++ tests/opponentTest.js
@@ -5,7 +5,7 @@ describe("opponent", function() {
   beforeEach(function() {
     opponent = new Opponent();
     ship = jasmine.createSpy('ship', { 'size' : 1 })
-    spyGrid = jasmine.createSpy('grid', { 'grid': ["X", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5",
+    opponentGrid = jasmine.createSpy('grid', { 'grid': ["X", "A2", "A3", "A4", "A5", "B1", "X", "X", "B4", "B5",
                     "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"]} )
   });
 
@@ -14,14 +14,11 @@ describe("opponent", function() {
   // })
 
   it('places a single small ship on the board upon creation', function(){
-    console.log(spyGrid.grid)
-    expect(spyGrid.grid[0]).toEqual('X')
+    expect(opponentGrid.grid[0]).toEqual('X')
   })
-  //
-  // it('places a single medium ship on the board upon creation', function(){
-  //   expect(opponent.board.grid[6]).toEqual('X')
-  //   expect(opponent.board.grid[7]).toEqual('X')
-  // })
 
-
+  it('places a single medium ship on the board upon creation', function(){
+    expect(opponentGrid.grid[6]).toEqual('X')
+    expect(opponentGrid.grid[7]).toEqual('X')
+  })
 });
