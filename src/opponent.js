b4fade2a036c4521d74716c177d7673ffd7e3de1
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-04 11:46:12 +0100

    Opponent class places one ship upon creation

--- src/opponent.js
+++ src/opponent.js
@@ -1,5 +1,14 @@
 class Opponent {
   constructor(){
     this.board = new Grid()
+    this.placeShip(new Ship(1), 'A1')
+
+  }
+
+  placeShip(ship, target){
+    var index = this.board.grid.findIndex(element => element == target)
+    for (var i = 0; i < ship.size; i++){
+      this.board.grid.splice(index + i, 1, 'X')
+    }
   }
 }
--- tests/opponentTest.js
+++ tests/opponentTest.js
@@ -3,11 +3,18 @@ describe("opponent", function() {
 
   beforeEach(function() {
     opponent = new Opponent();
-    grid = new Grid()
+    ship = jasmine.createSpy('ship', { 'size' : 1 })
+    grid = jasmine.createSpy('grid', { 'grid': ["X", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5",
+                    "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"]} )
   });
 
-  it('renders with an empty grid', function(){
-    expect(opponent.board).toEqual(grid)
+  // it('renders with an empty grid', function(){
+  //   expect(opponent.board).toEqual(grid)
+  // })
+
+  it('places a single ship on the board automatically', function(){
+    console.log(grid.grid)
+    expect(opponent.board.grid).toEqual(grid.grid)
   })
 
 });
