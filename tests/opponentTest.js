2809a1c4e648724f2282afa76171beef295b8483
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-04 16:13:33 +0100

    Remove unneccessary variable in opponent test

--- tests/opponentTest.js
+++ tests/opponentTest.js
@@ -3,7 +3,6 @@ describe("opponent", function() {
   var spyGrid;
 
   beforeEach(function() {
-    ship = jasmine.createSpy('ship', { 'size' : 1 })
     opponentGrid = jasmine.createSpy('grid', { 'grid': ["X", "A2", "A3", "A4", "A5", "B1", "X", "X", "B4", "B5",
                     "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"]} )
     opponent = new Opponent(opponentGrid);
