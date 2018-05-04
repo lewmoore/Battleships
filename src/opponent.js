68591fbfdd184ead2b73fb213ea2372c34ad3637
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-04 15:36:53 +0100

    Inject grid instance in to opponent class and use spy in opponent test

--- src/opponent.js
+++ src/opponent.js
@@ -1,13 +1,17 @@
 class Opponent {
-  constructor(){
-    this.board = new Grid()
+  constructor(grid = new Grid()){
+    this.board = grid
+    
     var target = this.targetGenerator()
     this.placeShip(new Ship(1), target)
+    var target = this.targetGenerator()
+    this.placeShip(new Ship(2), target)
+    var target = this.targetGenerator()
+    this.placeShip(new Ship(3), target)
   }
 
   placeShip(ship, target){
     var index = this.board.grid.findIndex(element => element == target)
-    console.log(target, 'PlaceShip Fn')
     for (var i = 0; i < ship.size; i++){
       this.board.grid.splice(index + i, 1, 'X')
     }
@@ -15,7 +19,6 @@ class Opponent {
 
   targetGenerator(){
     var target = this.board.grid[Math.floor(Math.random()*this.board.grid.length)]
-    console.log(target, 'Generator')
     return target
   }
 }
