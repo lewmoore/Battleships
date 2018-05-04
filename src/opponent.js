b25045c51ad283867368a7a6f1b2d9f0f63c566c
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-04 15:03:05 +0100

    Opponent class radomly generates a single target for ship placement

--- src/opponent.js
+++ src/opponent.js
@@ -1,14 +1,21 @@
 class Opponent {
   constructor(){
     this.board = new Grid()
-    this.placeShip(new Ship(1), 'A1')
-    this.placeShip(new Ship(2), 'B1')
+    var target = this.targetGenerator()
+    this.placeShip(new Ship(1), target)
   }
 
   placeShip(ship, target){
     var index = this.board.grid.findIndex(element => element == target)
+    console.log(target, 'PlaceShip Fn')
     for (var i = 0; i < ship.size; i++){
       this.board.grid.splice(index + i, 1, 'X')
     }
+  }
+
+  targetGenerator(){
+    var target = this.board.grid[Math.floor(Math.random()*this.board.grid.length)]
+    console.log(target, 'Generator')
+    return target
   }
 }
--- tests/opponentTest.js
+++ tests/opponentTest.js
@@ -9,16 +9,7 @@ describe("opponent", function() {
                     "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"]} )
   });
 
-  // it('renders with an empty grid', function(){
-  //   expect(opponent.board).toEqual(grid)
-  // })
-
   it('places a single small ship on the board upon creation', function(){
     expect(opponentGrid.grid[0]).toEqual('X')
-  })
-
-  it('places a single medium ship on the board upon creation', function(){
-    expect(opponentGrid.grid[6]).toEqual('X')
-    expect(opponentGrid.grid[7]).toEqual('X')
   })
 });
